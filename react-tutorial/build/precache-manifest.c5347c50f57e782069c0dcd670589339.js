fd65db2df7e4b2c38abf81538972ff706c6b2348
zengqingbo <zengqingbo@139.com>
2018-10-28 17:31:41 +0800

    react-tutor

---
+++ react-tutorial/build/precache-manifest.c5347c50f57e782069c0dcd670589339.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "6ff21ee3efd696d452cb",
+    "url": "./static/js/main.6ff21ee3.chunk.js"
+  },
+  {
+    "revision": "5b966dc2e69f15274453",
+    "url": "./static/js/1.5b966dc2.chunk.js"
+  },
+  {
+    "revision": "6ff21ee3efd696d452cb",
+    "url": "./static/css/main.a47cd517.chunk.css"
+  },
+  {
+    "revision": "0d1f1e25ec5a2c18ce53e901335eb3d4",
+    "url": "./index.html"
+  }
+];
